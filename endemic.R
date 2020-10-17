32a9d4dc92c5731f8368eea77cffef46af9a326f
Jonathan Dushoff <dushoff@mcmaster.ca>
2020-10-16 23:51:37 -0400

    good night

--- endemic.R
+++ endemic.R
@@ -1,5 +1,5 @@
-logR0 <- seq(log(0.5), log(8), length.out=100)
+logR0 <- seq(log(0.5), log(8), length.out=200)
 R0 <- exp(logR0)
 V <- 1-1/R0
 V <- ifelse(V<0, 0, V)
-plot(R0, V, type="l")
+plot(R0, V, type="l", log="x")
